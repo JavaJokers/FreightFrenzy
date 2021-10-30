46ef51c16b7a67b2682b6dc871437fc39f20e041
Sam948-byte <66854780+Sam948-byte@users.noreply.github.com>
2021-10-30 13:08:28 -0500

    Create MecanumFieldOriented.java

---
+++ TeleOp/MecanumFieldOriented.java
@@ -0,0 +1,13 @@
+
+
+
+
+
+// rotation 
+double x_rotated = x * Math.cos(angle) - y * Math.sin(angle);
+double y_rotated = x * Math.sin(angle) + y * Math.cos(angle);
+// x, y, theta input mixing
+frontLeftMotor.setPower(x_rotated + y_rotated + t);
+backLeftMotor.setPower(x_rotated - y_rotated + t);
+frontRightMotor.setPower(x_rotated - y_rotated - t);
+backRightMotor.setPower(x_rotated + y_rotated - t);
